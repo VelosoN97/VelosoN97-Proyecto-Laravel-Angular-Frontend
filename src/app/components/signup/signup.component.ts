1be6241e853386cf1e837f10e2b67d4958b91747
VelosoN97 <nicolas.veloso0397@gmail.com>
2025-03-09 20:14:05 -0300

    Noveno commit - Implementando toda la logica de funciones en los componentes TS

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  registerForm: FormGroup;
+  errors: any;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ){
+    this.registerForm = this.fb.group({
+      name: [''],
+      email: [''],
+      password: [''],
+      password_confirmation: ['']
+    });
+  }
+
+  onSubmit(): void {
+    this.cleanErrors();
+    this.authService.register(this.registerForm.value).subscribe(
+      response => this.handleResponse(response),
+      errors => this.handleErrors(errors)
+    );
+  }
+
+  private handleResponse(response: any): void{
+    console.log(response.message);
+
+  }
+
+  private handleErrors(errors: any): void{
+    this.errors = errors.error.errors;
+    console.log(this.errors);
+  }
+
+  private cleanErrors(): void{
+    this.errors = null;
+  }
 }
