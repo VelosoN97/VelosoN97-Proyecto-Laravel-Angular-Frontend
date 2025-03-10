2274d2cb3fd3d5fc9d49cb255df7cac19d37e70c
VelosoN97 <nicolas.veloso0397@gmail.com>
2025-03-10 13:37:01 -0300

    Commit 14 - Creación de la logica del comoponente TS de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { TokenService } from '../../services/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errors: any;
 
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private router: Router,
+    private fb: FormBuilder
+  ){
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+  }
+  
+  onSubmit(): void{
+    this.cleanErrors();
+    this.authService.login(this.loginForm.value).subscribe(
+      response => this.handleResponse(response),
+      errors => this.handleErrors(errors)
+    );
+  }
+
+  private handleResponse(response: any): void{
+    console.log(response.message);
+    this.tokenService.handleToken(response.token);
+  }
+
+  private handleErrors(errors: any): void{
+    this.errors = errors.error.errors;
+    console.log(this.errors);
+  }
+
+  private cleanErrors(): void{
+    this.errors = null;
+  }
 }
