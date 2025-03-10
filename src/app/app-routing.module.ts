25d2ba4e5715b0aa48c4e030d92883657278c924
VelosoN97 <nicolas.veloso0397@gmail.com>
2025-03-10 13:10:00 -0300

    Commit 13 - Definiendo rutas de acceso de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent  },
   { path: 'signup', component: SignupComponent }
 ];
 
